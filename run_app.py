d4d15a99bcfb8251c33ac3bbe42b822a9047d966
xiaozechong <xiaozechong@kboxsoft.com>
2026-06-05 11:32:13 +0800

    fix: 实现打包

--- run_app.py
+++ run_app.py
@@ -1,8 +1,12 @@
+import os
 import sys
+
+os.environ["STREAMLIT_GLOBAL_DEVELOPMENT_MODE"] = "false"
+
 from streamlit.web.cli import main as streamlit_main
+
 
 if __name__ == "__main__":
     # 模拟命令行参数：streamlit run main.py
     sys.argv = ["streamlit", "run", "main.py"] + sys.argv[1:]
-    print("sys.argv =", sys.argv)
     sys.exit(streamlit_main())
